c61402e537ce892d8caa6b42ddec60aa02aa7182
Cleiton Santos <90425855+Setyns@users.noreply.github.com>
2022-09-10 13:06:54 +0000

    car game guide by angulars youtube channel

---
+++ exemplo espera ENTER.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void EsperaEnter()
+{
+  int tecla;
+  printf("Pressione ENTER\n");
+  do
+    {
+      tecla = getchar();
+      if(tecla i=13)
+      {
+        printf("digite ENTER\n")
+      }
+    } while (tecla !=13);
+}
+void main(){
+  EsperaEnter();
+  EsperaEnter();
+}
